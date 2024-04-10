ce0fc3e21f8139c4d34ead9dc0eacc495e2ac3b9
ACernei <andreicernei9@gmail.com>
2024-04-10 03:13:15 +0300

    wip

--- EhrSystem/Infrastructure/Migrations/20240410002231_InitialCreate.Designer.cs
+++ EhrSystem/Infrastructure/Migrations/20240410002231_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240404130145_InitialCreate")]
+    [Migration("20240410002231_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -218,25 +218,25 @@ namespace Infrastructure.Migrations
                     b.HasData(
                         new
                         {
-                            Id = new Guid("08e46d4d-790a-4b87-98b4-87769a5b3950"),
+                            Id = new Guid("7210b422-f17b-4f70-9a09-8279cf011fd6"),
                             Name = "User",
                             NormalizedName = "User"
                         },
                         new
                         {
-                            Id = new Guid("f90d8268-41bb-4c5d-b4c0-1febe9266a8b"),
+                            Id = new Guid("0157547f-b451-4b84-95b0-4eb285a6d2dc"),
                             Name = "Doctor",
                             NormalizedName = "Doctor"
                         },
                         new
                         {
-                            Id = new Guid("be28b826-9df1-47b6-8d2f-6ed0b61949d2"),
+                            Id = new Guid("3646824b-419b-4b7e-835f-3a8169458a11"),
                             Name = "LabTechnician",
                             NormalizedName = "LabTechnician"
                         },
                         new
                         {
-                            Id = new Guid("522e1b2f-deb6-4b58-8501-98020c166905"),
+                            Id = new Guid("42e88610-3edf-4986-8a10-8ae204a9e9ef"),
                             Name = "Admin",
                             NormalizedName = "Admin"
                         });
